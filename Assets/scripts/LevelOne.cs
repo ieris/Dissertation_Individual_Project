5a73bebc1529bcfcdf4460dec2c581ddca8884a2
Iveta <stripeii@uni.coventry.ac.uk>
2017-04-10 17:40:10 +0100

    Moving Platform Movement

--- Assets/scripts/LevelOne.cs
+++ Assets/scripts/LevelOne.cs
@@ -21,39 +21,83 @@ public class LevelOne : MonoBehaviour
     public Button run;
     public GameObject platformOne;
     public GameObject platformTwo;
-    public GameObject platformThree;
+    public GameObject movingPlatform;
     public GameObject player;
+
+    //store coordinatees
+    private Vector3 movingPlatformPos;
 
     //split the string to check if object name exists
     private string dot = ".";
     private string equals = "=";
     private string semicolon = ";";
-    private string coordinate;
+    private string enteredCoordinate;
+    private float coordinateFloat;
+    private float finalCoordinate;
     private string[] array;
     
 
     //levelComplete
     private bool levelCompleted = false;
+    private bool movingPlatformLower = false;
+    private bool movingPlatformHigher = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         run.onClick.AddListener(onRunClick);
         input = GetComponent<InputField>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
 
+        //Store object coordinates
+        movingPlatformPos = movingPlatform.transform.position;
+    }
+
+    void Update ()
+    {
+        //going down
+        Debug.Log("coordinate " + coordinateFloat);
+        if (movingPlatformLower)
+        {
+            //Move down until it reaches the final coordinate
+            if (movingPlatform.transform.position.y > finalCoordinate)
+            {
+                movingPlatform.transform.position += Vector3.down * coordinateFloat * Time.deltaTime;
+            }
+            else if (movingPlatform.transform.position.y <= finalCoordinate)
+            {
+                movingPlatform.transform.position = new Vector3(movingPlatform.transform.position.x, finalCoordinate, movingPlatform.transform.position.z);
+                levelCompleted = true;
+            }
+        }
+
+        //going up
+        else if(movingPlatformHigher)
+        {
+            //Move up until it reaches the final coordinate
+            if (movingPlatform.transform.position.y < finalCoordinate)
+            {
+                Debug.Log("going up");
+                movingPlatform.transform.position += Vector3.up * coordinateFloat * Time.deltaTime;
+            }
+            else if (movingPlatform.transform.position.y >= finalCoordinate)
+            {
+                movingPlatform.transform.position = new Vector3(movingPlatform.transform.position.x , finalCoordinate, movingPlatform.transform.position.z);
+                levelCompleted = true;
+            }
+        }
     }
 
     void onRunClick()
     {
+        Debug.Log("y" + movingPlatform.transform.position.y);
         inputCopy = input.text;
         Debug.Log("Run was clicked! :D");
         inputCopy = Regex.Replace(inputCopy, @"\s", string.Empty);  //remove spaces
 
+        if(input == null)
+        {
+            Debug.Log("Input field is empty");
+        }
         if (inputCopy.EndsWith(semicolon))                          //check for semicolon
         {
             Debug.Log("Semicolon is here! :D");
@@ -74,36 +118,41 @@ public class LevelOne : MonoBehaviour
             { 
                 if (str.Contains(array[0]))
                 {
-                    Debug.Log(array[0] + " Object exists! :D");
-                    levelCompleted = true;                              //everything is correct
+                    Debug.Log(array[0] + " Object exists! :D");                    
+
+                    int coordinatePosition = inputCopy.IndexOf(equals);
+                    enteredCoordinate = inputCopy.Substring(coordinatePosition + 1, inputCopy.Length - 2 - coordinatePosition);
+
+                    //store coordinate value
+                    coordinateFloat = float.Parse(enteredCoordinate);
+
+                    if (Regex.IsMatch(inputCopy, @"[\w]+\S\.([x|y])(\+)=\d+;"))      //match regex movingPlatform.y+=100;
+                    {
+                        Debug.Log("Going up");
+                        finalCoordinate = movingPlatformPos.y + coordinateFloat;
+                        movingPlatformHigher = true;
+                    }
+                    else if (Regex.IsMatch(inputCopy, @"[\w]+\S\.([x|y])(-)=\d+;")) //match regex movingPlatform.y-=100;
+                    {
+                        Debug.Log("Going down");
+                        finalCoordinate = movingPlatformPos.y - coordinateFloat;
+                        movingPlatformLower = true;
+                    }
                 }
                 else
                 {
                     Debug.Log("The object name does not exist");
                 }
             }
-            
-            //Debug.Log("input " + inputCopy);
-            //Debug.Log("hello");
         }
         else if (Regex.IsMatch(inputCopy, @"[\w]+\S\.([x|y])=\d+;"))    //match regex movingPlatform.y=100;
         {
             Debug.Log("You are setting the coordinate instead of increasing/decreasing it");
         }
-
-        if(levelCompleted)
-        {
-            int coordinatePosition = inputCopy.IndexOf(equals);
-            coordinate = inputCopy.Substring(coordinatePosition + 1, inputCopy.Length - 2 - coordinatePosition);
-
-            Debug.Log(coordinate);
-            moveObject(Convert.ToInt32(coordinate));
-
-        }
     }
 
-    void moveObject(int coord)
+    void moveObject(float coord)
     {
-
+        movingPlatform.transform.Translate(0,coord * Time.deltaTime, 0);
     }
 }
