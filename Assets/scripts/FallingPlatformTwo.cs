7836bd32d71a4ce7768f27972329dfb61577b73e
Iveta <stripeii@uni.coventry.ac.uk>
2017-04-12 15:59:53 +0100

    Level Three platforms are falling

---
+++ Assets/scripts/FallingPlatformTwo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatformTwo : MonoBehaviour
+{
+    public GameObject platform;
+
+    private bool fallOne = false;
+    private float time = 0.5f;
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(name + " collided");
+        if (col.gameObject.tag == "fallingPlatformTwo")
+        {
+            platform = col.gameObject;
+            Debug.Log("Collided One! :D");
+            fallOne = true;
+        }
+    }
+
+    void Update()
+    {
+        if (fallOne)
+        {
+            if (time >= 0)
+            {
+                //Debug.Log(time);
+                time -= Time.deltaTime;
+            }
+            else
+            {
+                platform.AddComponent<Rigidbody>();
+                time = 0.5f;
+                fallOne = false;
+            }
+        }
+    }
+}
