27f8f2b68957e5fd12cacf413ba9051f250c5000
Iveta <stripeii@uni.coventry.ac.uk>
2017-04-20 23:54:19 +0100

    Working on for loop tutorial

--- Assets/scripts/tutorials/levelOneTutorial.cs
+++ Assets/scripts/tutorials/levelOneTutorial.cs
@@ -9,8 +9,11 @@ public class levelOneTutorial : MonoBehaviour
     public Text tutorialTitle;
     public Text tutorialUnderline;
     public Text tutorialMessage;
+    public Text tutorialMessagePartTwo;
+    public Text tutorialMessagePartThree;
     public Button dismissButton;
     public Image tutorialImage;
+    public Image tutorialImage2;
     public Text dissmissButtonText;
 
     //tasks
@@ -26,10 +29,12 @@ public class levelOneTutorial : MonoBehaviour
         tutorialTitle.GetComponent<Text>().enabled = false;
         tutorialUnderline.GetComponent<Text>().enabled = false;
         tutorialMessage.GetComponent<Text>().enabled = false;
+        tutorialMessagePartTwo.GetComponent<Text>().enabled = false;
+        tutorialMessagePartThree.GetComponent<Text>().enabled = false;
         dismissButton.GetComponent<Button>().enabled = false;
         dissmissButtonText.GetComponent<Text>().enabled = false;
         tutorialImage.GetComponent<Image>().enabled = false;
-
+        tutorialImage2.GetComponent<Image>().enabled = false;
         dismissButton.onClick.AddListener(onDismissClick);
 
         //taskOne = true;
@@ -56,12 +61,14 @@ public class levelOneTutorial : MonoBehaviour
             tutorialBox.GetComponent<MeshRenderer>().enabled = true;
             tutorialTitle.GetComponent<Text>().enabled = true;
             tutorialUnderline.GetComponent<Text>().enabled = true;
-            tutorialMessage.GetComponent<Text>().enabled = true;
+            tutorialMessagePartTwo.GetComponent<Text>().enabled = true;
+            tutorialMessagePartThree.GetComponent<Text>().enabled = true;
             dismissButton.GetComponent<Button>().enabled = true;
             dissmissButtonText.GetComponent<Text>().enabled = true;
-            //tutorialImage.GetComponent<Image>().enabled = true;
+            tutorialImage2.GetComponent<Image>().enabled = false;
 
-            tutorialMessage.text = "Try to move the box to the x axis by 2 units using:\n\nfor(int i = 0; i < 8; i++)\n{\n\tbox.x ++;\n}";
+            tutorialMessagePartTwo.text = "A for loop can be used to move the box to the exit. It is a bit like counting. It starts counting from 0 and each time you count, you can execute a piece of code to move the box by 1 unit. It repeats this until the loop is done counting.";
+            tutorialMessagePartThree.text = "Try it out:\n\nfor(int i = 0; i < 8; i++)\n{\n\tbox.x ++;\n}\nRemember the counter starts at zero. If you want to move the box by 5 units, you need to use 'i < 4' inside the for loop.";
         }
         if(hide)
         {
@@ -70,9 +77,12 @@ public class levelOneTutorial : MonoBehaviour
             tutorialTitle.GetComponent<Text>().enabled = false;
             tutorialUnderline.GetComponent<Text>().enabled = false;
             tutorialMessage.GetComponent<Text>().enabled = false;
+            tutorialMessagePartTwo.GetComponent<Text>().enabled = false;
+            tutorialMessagePartThree.GetComponent<Text>().enabled = false;
             dismissButton.GetComponent<Button>().enabled = false;
             dissmissButtonText.GetComponent<Text>().enabled = false;
             tutorialImage.GetComponent<Image>().enabled = false;
+            tutorialImage2.GetComponent<Image>().enabled = false;
         }
 
 	}
@@ -85,9 +95,12 @@ public class levelOneTutorial : MonoBehaviour
         tutorialTitle.GetComponent<Text>().enabled = false;
         tutorialUnderline.GetComponent<Text>().enabled = false;
         tutorialMessage.GetComponent<Text>().enabled = false;
+        tutorialMessagePartTwo.GetComponent<Text>().enabled = false;
+        tutorialMessagePartThree.GetComponent<Text>().enabled = false;
         dismissButton.GetComponent<Button>().enabled = false;
         dissmissButtonText.GetComponent<Text>().enabled = false;
         tutorialImage.GetComponent<Image>().enabled = false;
+        tutorialImage2.GetComponent<Image>().enabled = false;
 
         //taskOne = false;
     }
